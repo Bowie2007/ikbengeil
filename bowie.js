a337ed80ac1124e6d88e0664251d146b3d380f00
bowiepussylover69 <122539452+Bowie2007@users.noreply.github.com>
2023-02-02 15:04:26 +0100

    Create bowie.js

---
+++ bowie.js
@@ -0,0 +1,7 @@
+var drum1 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/2636[kb]065_relaxed-roller.wav.mp3");
+var drum2 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/1658[kb]050_glitch-hop-serenade.wav.mp3");
+var drum3 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/823[kb]050_udu_groove-1.wav.mp3");
+var drum4 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/354[kb]059_808-hiphop.wav.mp3");
+var drum5 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/354[kb]059_808-hiphop.wav.mp3");
+var drum6 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/354[kb]059_808-hiphop.wav.mp3");
+
